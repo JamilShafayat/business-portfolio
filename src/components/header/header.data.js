2308116888bae6627b3088bb5f7c3429e7d566db
jamilshafayat <jamilshafayat8@gmail.com>
2021-07-12 23:40:39 +0600

    header data shows

--- src/components/header/header.data.js
+++ src/components/header/header.data.js
@@ -1,18 +1,20 @@
-export default [
-  {
-    path: 'home',
-    label: 'Home',
-  },
-  {
-    path: 'feature',
-    label: 'Features',
-  },
-  {
-    path: 'pricing',
-    label: 'Pricing',
-  },
-  {
-    path: 'testimonial',
-    label: 'Testimonial',
-  },
+const menuItems = [
+	{
+		path: "home",
+		label: "Home",
+	},
+	{
+		path: "feature",
+		label: "Features",
+	},
+	{
+		path: "pricing",
+		label: "Pricing",
+	},
+	{
+		path: "testimonial",
+		label: "Testimonial",
+	},
 ];
+
+export default menuItems;
